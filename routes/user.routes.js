3b65386b3ac4d44cfc79ed268bb174f3f781119b
varunrajput27 <varunaarya709@gmail.com>
2025-08-22 12:49:46 +0530

    added update and delete to userroutes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,6 +6,8 @@ const authController = require('../controllers/user.controller');
 
 router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
+router.put('/update/:id', authController.updateUser);
+router.delete('/delete/:id', authController.deleteUser);
 
 
 module.exports=router;
