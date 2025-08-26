200e9990ecd34b60630289422db17340066df65b
varunrajput27 <varunaarya709@gmail.com>
2025-08-26 12:16:41 +0530

    add getpropertybyId route

--- routes/properties.routes.js
+++ routes/properties.routes.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get("/all", propertiesController.getAllProperties);
 router.post("/add", propertiesController.upload.array("images", 10), propertiesController.addProperty);
 router.get("/get_number", propertiesController.getNumberOfProperties);
+router.get("/:id", propertiesController.getPropertyById);
 
 
 
