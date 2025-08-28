6cd9a1b547f1eea4328a003b5de75e5cc56e9780
varunrajput27 <varunaarya709@gmail.com>
2025-08-28 11:58:32 +0530

    update login response

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -54,7 +54,7 @@ const loginUser = async (req, res) => {
     res.status(200).json({
       message: "User logged in successfully",
       token,
-      user: { id: user._id, name: user.name, phone: user.phone },
+      user: { id: user._id, fullname: user.fullname,  email: user.email, phone: user.phone },
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
