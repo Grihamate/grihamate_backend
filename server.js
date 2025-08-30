a537c40d0e46da1d1a1a6624922dd1d5acf4c77d
Chandan <cs762585@gmail.com>
2025-08-30 11:29:37 +0530

    added cors

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: [
+    "http://localhost:5173",
+    "https://grihamate-frontend-vite-repo.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
