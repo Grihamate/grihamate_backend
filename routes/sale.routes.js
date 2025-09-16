a67b0a7dbe57f98f0e8f3032137fac5d2c20a52f
varunrajput27 <varunaarya709@gmail.com>
2025-09-16 16:36:00 +0530

    update owner number route

--- routes/sale.routes.js
+++ routes/sale.routes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post("/add",authMiddleware, saleController.upload.array("images", 10), saleController.addSaleProperty);
 router.get("/all", saleController.getAllSaleProperties);
-router.get("/get_number", saleController.getPropertyOwnerNumber);
+router.get("/get_number",authMiddleware, saleController.getPropertyOwnerNumber);
 router.get("/search",authMiddleware , saleController.searchSaleProperties);
 router.get("/:id",authMiddleware, saleController.getSalePropertyById);
 
